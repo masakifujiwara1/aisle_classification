64c410af00c49621b7d0a700e45eda883b0a9e58
MasakiFujiwara <fmasaki2020@gmail.com>
2022-08-30 19:37:21 +0900

    update net

---
+++ src/aisle_class_net.py
@@ -0,0 +1,29 @@
+from asyncore import write
+from itertools import count
+from platform import release
+from pyexpat import features, model
+import numpy as np
+import matplotlib as plt
+import os
+import time
+from os.path import expanduser
+
+
+import torch
+import torchvision
+import torch.nn as nn
+from torch.utils.data import DataLoader, TensorDataset, Dataset, random_split
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+import torch.optim as optim
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from yaml import load
+
+
+class Net(nn.Module):
+    def __init__(self, n_channnel, n_out):
+        super().__init__()
+        self.conv1 = nn.Conv2d(3, )
